bb1ea8c60f9f8345903f93288bdad1188dcc946a
bruor <adrien.carlyle@gmail.com>
2023-11-04 02:46:03 -0400

    Update filteranddedupe.js

--- filteranddedupe.js
+++ filteranddedupe.js
@@ -1,65 +1,65 @@
-// VERSION 1.0
+// VERSION 1.4
 // require('../Tools/pluginsHelper');
 
-// Define the grouptitlefilter
-var grouptitlefilter = ["africa", "albania", "arabic", "australia", "belgium", "brazil", "canada french", "caribbean", "ex-ussr", "ex-yu", "filipino", "france", "germany", "greece", "india", "israel", "italy", "latino", "macedonia", "netherlands", "nordic", "poland", "portuguese", "romania", "russia", "spain"];
-
 // Playlist is available in m3u8 variable as Buffer object
-// Split the M3U8 content by lines
-var lines = m3u8.toString().split('\n');
 
-// Initialize an array to store channel objects
-var channels = [];
+//	For logging to hls-proxy console use:
+//	globals.error('Your text of message');
+//  globals.error(`Your text of message: ${variable}`);
 
-// Regular expression to extract channel information
-const regex = /#EXTINF:-1 tvg-id="([^"]+)" tvg-name="([^"]+)" tvg-type="([^"]+)" group-title="([^"]+)" tvg-logo="([^"]+),(.*)/;
+// Define the grouptitlefilter
+const grouptitlefilter = ["africa", "albania", "arabic", "australia", "belgium", "brazil", "canada french", "caribbean", "ex-ussr", "ex-yu", "filipino", "france", "germany", "greece", "india", "israel", "italy", "latino", "macedonia", "netherlands", "nordic", "poland", "portuguese", "romania", "russia", "spain"];
 
-// Loop through the lines to extract channel information
-for (var i = 0; i < lines.length; i += 2) {
-  if (i + 1 < lines.length) {
-    var match = lines[i].match(regex);
-    if (match) {
-      let channel = {
-        tvgid: match[1],
-        tvgname: match[2],
-        tvgtype: match[3],
-        grouptitle: match[4],
-        tvglogo: match[5],
-        name: match[6],
-        url: lines[i + 1]
-      };
-      channels.push(channel);
+let linesArray = m3u8.toString().split('\n');
+let skipChannels = [];
+
+for (var i = 0; i < linesArray.length; i += 1) {
+  var line = linesArray[i];
+  var match = /group-title="([^"]+)"/.exec(line);
+  if (match) {
+    var groupTitle = match[1].toLowerCase();
+    if (grouptitlefilter.includes(groupTitle)) {
+      var tvgIdMatch = /tvg-id="([^"]+)"/.exec(line);
+      if (tvgIdMatch) {
+        var tvgId = tvgIdMatch[1];
+        skipChannels.push(tvgId);
+      }
     }
   }
 }
 
-// Loop through the channels array and remove objects with matching grouptitles
-for (var i = channels.length - 1; i >= 0; i--) {
-  var channel = channels[i];
-  if (grouptitlefilter.includes(channel.grouptitle.toLowerCase())) {
-    channels.splice(i, 1); // Remove the channel if the grouptitle is in the filter
+for ( var i = linesArray.length-2; i > 0; i--) {	
+  var line = linesArray[i];  
+  var tvgIdMatch = /tvg-id="([^"]+)"/.exec(line);
+  if (tvgIdMatch) {
+    if (skipChannels.includes(tvgIdMatch[1])){
+      linesArray.splice(i, 2); // Remove the line with tvg-id and the next line.
+    }
   }
 }
 
-// Create a Set to track unique tvgid values
-let uniqueTvgIds = new Set();
 
-// Filter and keep only unique channels based on their tvgid
-let uniqueChannels = channels.filter(channel => {
-  if (!uniqueTvgIds.has(channel.tvgid)) {
-    uniqueTvgIds.add(channel.tvgid);
-    return true;
+var uniqueLineIDs = {}; // Object to store unique lines
+var uniqueLines = [];   //output array
+uniqueLines.push('#EXTM3U\n')  
+
+for (var i = 0; i < linesArray.length; i++) {
+  var line = linesArray[i];
+  if (line.trim() === "") {
+      // Skip empty lines
+      continue;
   }
-  return false;
-});
+  var tvgIdMatch = /tvg-id="([^"]+)"/.exec(line);  // Extract tvg-id value
+  if (tvgIdMatch) {
+	if (!uniqueLineIDs[tvgIdMatch[1]]) {
+      uniqueLineIDs[tvgIdMatch[1]] = true;
+      uniqueLines.push(line, linesArray[i + 1]);
+	}
+  }
+}
+globals.error(`${uniqueLines.length} lines in final m3u`);
 
-// Recompose the uniqueChannels array into M3U8 format
-let recomposedM3U8 = '#EXTM3U\n';
-uniqueChannels.forEach(channel => {
-  recomposedM3U8 +=
-    `#EXTINF:-1 tvg-id="${channel.tvgid}" tvg-name="${channel.tvgname}" tvg-type="${channel.tvgtype}" group-title="${channel.grouptitle}" tvg-logo="${channel.tvglogo}",${channel.name}\n` +
-    `${channel.url}\n`;
-});
 
-// Output the recomposed M3U8
-print(recomposedM3U8);
+for (let i = 0; i < uniqueLines.length; i++) {
+print(uniqueLines[i] + '\n');
+}
